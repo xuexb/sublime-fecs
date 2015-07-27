c44cb7b96551bd43fbd18f54ebadf1b3723d7e7b
xiaowu <fe.xiaowu@gmail.com>
2015-07-27 09:08:59 +0800

    添加文件

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# sublime-fecs
-fecs for sublime
+# fecs for sublime
+
+依赖`fecs`的sublime小插件，支持`check`,`format`命令
+
+## install
+
+```
+$ npm install fecs -g
+$ git clone https://github.com/xuexb/sublime-fecs.git
+$ cp -rf ./sublime-fecs/* %sublime packages%/fecs/
+```
---
+++ fecs.py
@@ -0,0 +1,46 @@
+import sublime
+import sublime_plugin
+
+
+class fecsCheckCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        filepath = self.view.file_name()
+        packages = sublime.packages_path()
+        args = {
+            "cmd": [
+                "fecs",
+                filepath,
+            ],
+            "file_regex": r"fecs: (.+)\]",
+            "line_regex": r"(\d+),(\d+): (.*)$"
+        }
+
+        if sublime.platform() == "windows":
+            args['cmd'][0] += ".cmd"
+        elif sublime.platform() == "osx":
+            args['path'] = "/usr/local/share/npm/bin:/usr/local/bin:/opt/local/bin"
+
+        self.view.window().run_command('exec', args)
+
+
+class fecsFormatCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        filepath = self.view.file_name()
+        packages = sublime.packages_path()
+        args = {
+            "cmd": [
+                "fecs",
+                "format",
+                filepath,
+                "--replace"
+            ],
+            "file_regex": r"fecs: (.+)\]",
+            "line_regex": r"(\d+),(\d+): (.*)$"
+        }
+
+        if sublime.platform() == "windows":
+            args['cmd'][0] += ".cmd"
+        elif sublime.platform() == "osx":
+            args['path'] = "/usr/local/share/npm/bin:/usr/local/bin:/opt/local/bin"
+
+        self.view.window().run_command('exec', args)
